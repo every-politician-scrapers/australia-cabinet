fd25e742fdcde9515544d421a1910fc59df717f3
Tony Bowden <tony@tmtm.com>
2022-03-22 08:09:17 +0200

    compare QLD Chief Justices

---
+++ sources/qld-chief-justice/scraper.rb
@@ -0,0 +1,28 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Term'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[name dates].freeze
+    end
+
+    def name_node
+      name_cell.css('a').first
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
