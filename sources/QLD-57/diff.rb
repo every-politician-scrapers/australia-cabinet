62bfb9efdbe6327dcfb57afd858b05dd8753ab39
Tony Bowden <tony@tmtm.com>
2022-04-14 15:27:14 +0300

    refresh QLD 57

--- sources/QLD-57/diff.rb
+++ sources/QLD-57/diff.rb
@@ -5,7 +5,7 @@ require 'every_politician_scraper/comparison'
 
 class Comparison < EveryPoliticianScraper::NulllessComparison
   def columns
-    super + %i[psid]
+    super - %i[partylabel] + %i[psid]
   end
 end
 
