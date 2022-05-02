b5625546ccbfc789a4bdea59bdc6a4911e91f87b
Tony Bowden <tony@tmtm.com>
2022-05-02 09:38:53 +0300

    compare enwiki Defence Ministers

---
+++ sources/minister-defence/scraper.rb
@@ -0,0 +1,32 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Term'
+  end
+
+  def table_number
+    2
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no name color party pm start end].freeze
+    end
+
+    def name_node
+      name_cell.css('a').first
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
